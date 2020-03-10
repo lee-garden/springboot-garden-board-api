2e978c7ead56a1e7b7410a3066fb1696f6a724f5
GardenLee <khjvvv7@gmail.com>
2020-03-11 02:02:15 +0900

    Swagger 설정

--- src/main/java/com/gardeny/gardenboard/springboot/web/PostApiController.java
+++ src/main/java/com/gardeny/gardenboard/springboot/web/PostApiController.java
@@ -2,6 +2,8 @@ package com.gardeny.gardenboard.springboot.web;
 
 import com.gardeny.gardenboard.springboot.service.contents.PostService;
 import com.gardeny.gardenboard.springboot.web.dto.PostSaveRequestDto;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,8 +15,10 @@ public class PostApiController {
 
     private final PostService postService;
 
+    @ApiOperation(value = "포스트 생성 API")
     @PostMapping("/api/v1/post")
-    public Long save(@RequestBody PostSaveRequestDto requestDto) {
+    public Long save(@ApiParam(value = "category : NORMAL - > 일반 게시판", required = false)
+                     @RequestBody PostSaveRequestDto requestDto) {
         return postService.save(requestDto);
     }
 

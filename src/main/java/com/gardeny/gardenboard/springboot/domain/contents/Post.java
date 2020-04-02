c8bd3c97c7315d1cf5892b5ac70aea1f571277d9
GardenLee <khjvvv7@gmail.com>
2020-04-03 00:21:15 +0900

    Add auditing fields to Post
    - Post entity - BaseTimeEntity 상속

--- src/main/java/com/gardeny/gardenboard/springboot/domain/contents/Post.java
+++ src/main/java/com/gardeny/gardenboard/springboot/domain/contents/Post.java
@@ -1,5 +1,6 @@
 package com.gardeny.gardenboard.springboot.domain.contents;
 
+import com.gardeny.gardenboard.springboot.domain.BaseTimeEntity;
 import com.gardeny.gardenboard.springboot.domain.account.User;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,7 +11,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Post {
+public class Post extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/gardeny/gardenboard/springboot/web/contents/dto/PostListResponseDto.java
+++ src/main/java/com/gardeny/gardenboard/springboot/web/contents/dto/PostListResponseDto.java
@@ -6,6 +6,8 @@ import com.gardeny.gardenboard.springboot.domain.contents.Category;
 import com.gardeny.gardenboard.springboot.domain.contents.Post;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class PostListResponseDto {
     private Long id;
@@ -14,6 +16,7 @@ public class PostListResponseDto {
     private String user;
     private Category category;
     private String content;
+    private LocalDateTime created;
 
     public PostListResponseDto(Post entity) {
         this.id = entity.getId();
@@ -22,5 +25,6 @@ public class PostListResponseDto {
         this.user = entity.getUser().getName();
         this.category = entity.getCategory();
         this.content = entity.getContent();
+        this.created = entity.getCreated();
     }
 }
